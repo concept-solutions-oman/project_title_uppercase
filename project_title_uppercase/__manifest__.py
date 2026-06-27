0421167df67c12d09b964aa5bde54eda42355252
Rajesh <rajeshmave@gmail.com>
2026-06-27 11:26:38 +0530

    ssqsqsqsqss

---
+++ project_title_uppercase/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Project Title Uppercase',
+    'version': '1.0',
+    'category': 'Services/Project',
+    'summary': 'Automatically converts project titles to uppercase',
+    'description': """
+        This module automatically converts any lowercase text entered 
+        in the Project Title field to uppercase.
+    """,
+    'author': 'Concept Solutions',
+    # 'depends': ['concept_project_management'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ project_title_uppercase/models/project_csl.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class ProjectCsl(models.Model):
+    _inherit = 'project.csl'
+
+    @api.onchange('name')
+    def _onchange_name_uppercase(self):
+        """Convert project title to uppercase on change"""
+        if self.name:
+            self.name = self.name.upper()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Convert project title to uppercase on create"""
+        for vals in vals_list:
+            if vals.get('name'):
+                vals['name'] = vals['name'].upper()
+        return super(ProjectCsl, self).create(vals_list)
+
+    def write(self, vals):
+        """Convert project title to uppercase on write"""
+        if vals.get('name'):
+            vals['name'] = vals['name'].upper()
+        return super(ProjectCsl, self).write(vals)
